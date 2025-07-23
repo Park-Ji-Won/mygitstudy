4cbf5b9465ce62fa61fefff173efe403111876fa
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-23 19:36:14 +0900

    자료형

---
+++ py_practice/Data_Ty1.py
@@ -0,0 +1,13 @@
+t=input("정수를 입력하시오: ")
+x=int(t)
+
+t=input("실수를 입력하시오: ")
+y=float(t)
+
+print(x+y)
+
+print(type("Hello World"))
+print(type(3.2))
+print(type(17))
+
+print('나는 현재', str(21), '살 이다')
