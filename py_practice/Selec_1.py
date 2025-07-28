ef3a2c6d2173d899681f65ea9d66d5f5a9eef2ee
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-28 12:48:41 +0900

    if-else 문 and 조건문

---
+++ py_practice/Selec_1.py
@@ -0,0 +1,15 @@
+score=int(input("성적을 입력하시오: "))
+
+if score >= 60:
+    print("합격입니다.")
+    print("장학금도 수령가능합니다.")
+else:
+    print("불합격입니다.")
+
+num=int(input("정수를 입력하시오: "))
+
+if num%2 == 0:
+    print("짝수입니다.")
+    print("당신은 짝수인들끼리 수업을 수행합니다.")
+else:
+    print("홀수입니다.")    
