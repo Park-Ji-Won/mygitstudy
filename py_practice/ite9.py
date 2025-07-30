1f186b12670c7eeeb4e11e151eeaa6d9aa2c4a63
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 20:15:21 +0900

    기존의 조건문에서 수정

--- py_practice/ite9.py
+++ py_practice/ite9.py
@@ -1,5 +1,5 @@
 while True:
-    light=input('신호등의 색상을 입력하시오')
-    if light=='red':
-        break
+    light=input('신호등의 색상을 입력하시오')#반복문장
+    if light=='blue':#if 조건
+        break #루프에서 강제적으로 빠져나오는 문장
 print("전진!!")
