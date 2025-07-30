46836ea52cca1e26c843cb0cc26bfc204ceaebaf
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 19:43:19 +0900

    약간의 코드수정과 주석수정

--- py_practice/ite6.py
+++ py_practice/ite6.py
@@ -3,7 +3,8 @@ while response=="아니": # while 조건=>반복을 하는 조건이다.조건�
     response=input("엄마, 다됐어?")#반복되는 문장이다.
 print("먹자")
 
+#조건제어루프문 알고리즘
 passward=""
-while passward !="pythonsifun":
+while passward !="pythonisfun":#사용자가 몇번만에 로그인에 성공할줄 몰라 해당 조건제어가 사용됨
     passward=input("암호를 입력하시오: ")
 print("로그인 성공")
