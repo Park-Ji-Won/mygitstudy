d10f5340e25d3da597fdc86d5db7158a75ca4f09
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 18:18:01 +0900

    if-elif-else문을 활용한 점수기

---
+++ py_practice/Selec_9.py
@@ -0,0 +1,12 @@
+score=int(input("점수를 입력하시오: "))
+
+if score > 90:
+    print("A 입니다")
+elif score > 80:
+    print("B 입니다")
+elif score > 70:
+    print("C 입니다")
+elif score > 60:
+    print("D 입니다")
+else:
+    print("F 입니다")
