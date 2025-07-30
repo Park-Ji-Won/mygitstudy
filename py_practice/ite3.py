4cd983c707b2098dca0705f577b1b2bf88ae3549
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 19:06:48 +0900

    range() 함수 활용법

---
+++ py_practice/ite3.py
@@ -0,0 +1,5 @@
+for i in range(1,6,1):#range(start=0,stop,step=1)/range(시작값,종료값이나 stop은 미포함,한번에 증가되는 값)
+    print(i, end=" ")#end=""와 같이 지정하면 줄이 바뀌지 않은 상태로 전체 출력
+
+for i in range(10,-2,-1):
+    print(i, end=" ")
