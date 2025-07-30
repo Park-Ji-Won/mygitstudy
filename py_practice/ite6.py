091dda9b19851aa7beabb4939431579e9d1b011e
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 19:37:45 +0900

    while() 함수의 조건 제어 반복과 해당 함수를 활용한 조건제어루프 알고리즘

---
+++ py_practice/ite6.py
@@ -0,0 +1,9 @@
+response="아니"
+while response=="아니": # while 조건=>반복을 하는 조건이다.조건이 참이면 반복을 계속한다.
+    response=input("엄마, 다됐어?")#반복되는 문장이다.
+print("먹자")
+
+passward=""
+while passward !="pythonsifun":
+    passward=input("암호를 입력하시오: ")
+print("로그인 성공")
