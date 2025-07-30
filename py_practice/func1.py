4c421518b3a3a5108b27cc21e1edd0844f796cc0
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-30 20:23:00 +0900

    첫함수작성과 호출

---
+++ py_practice/func1.py
@@ -0,0 +1,7 @@
+def print_address(): # def키워드를 사용한 함수정의 #:=>코드블록 등장
+    print("강원특별자치도 원주시 교동 45번지")
+    print("Monty Python 본사")
+    print("허셸 폰 클라우스 슈타우펜베르크")
+    #함수를 구성하는 문장들 들여쓰기 필수
+
+print_address()#정의 해당 함수 호출
