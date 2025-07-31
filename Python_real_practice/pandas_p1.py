f266389817267fea1ca297b203fdadaa943f9c5d
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-31 16:55:44 +0900

    Pandas 개념 3가지 방식으로 DataFrame 객체 생성하기 실험

---
+++ Python_real_practice/pandas_p1.py
@@ -0,0 +1,15 @@
+# 리스트를 사용하여 DataFrame 객체 생성하기
+import pandas as pd
+
+data = [['A',1],['B',2],['C',3]]
+df=pd.DataFrame(data,columns=['col1','col2'])
+print(df)
+
+# 딕셔너리를 사용하여 DataFrame 객체 생성하기
+data = {'col1':['A','B','C'],'col2':[1,2,3]}
+df=pd.DataFrame(data)
+print(df)
+
+# CSV 파일을 사용하여 DataFrame 객체 생성하기
+df=pd.read_csv('data.csv')
+print(df)
