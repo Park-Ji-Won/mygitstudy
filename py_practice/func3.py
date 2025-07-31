4e9f2df9e5483eebbfd9c76f9a681feff364cbaf
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-31 21:03:41 +0900

    매개변수와 인수에 관해서 주석 추가 및 코드 수정

--- py_practice/func3.py
+++ py_practice/func3.py
@@ -1,7 +1,11 @@
-def get_sum(start,end):
+def get_sum(start,end):#매개변수
     sum=0
     for i in range(start,end+1):
         sum += i
     return sum
 
-print(get_sum(1,10))
+print(get_sum(1,10))#print(인수)
+
+#주의 매개변수의 개수는 정확히 인수에 맞춰서 일치해야 한다 
+#EX)매개변수가 두개 따라서 인수도 두개
+#해당 문제를 그대로 방치하고 지나간다면 아주 찾기 어려운 오류가 발생
