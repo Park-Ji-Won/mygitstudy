bd1001ace4af0d39835826be39565ef34f4e1024
Park-Ji-Won <dpfmwptm01@gamil.com>
2025-08-14 19:39:59 +0900

    About File I/O and writing and modify and print

---
+++ py_practice2.py/pygem6.py
@@ -0,0 +1,18 @@
+#File (I/O) 읽고 쓰기
+
+#해당 파일 이름으로 파일을 쓰기 모드로 연다
+with open('my_file.txt','w',encoding='utf-8') as f:
+    f.write("Hello, Mister John Doe!\n")
+    f.write("We are really miss you!\n")
+    
+with open('my_file.txt','a',encoding='utf-8') as f:
+    f.write("We are waiting for defence front line\n")
+    f.write("Please give me the order")
+    
+#방금 만든 해당 파이을 읽기모드로 연다
+with open('my_file.txt','r',encoding='utf-8') as f:
+    content=f.read()
+    print("--- File Info ---")
+    print(content)
+    
+    
